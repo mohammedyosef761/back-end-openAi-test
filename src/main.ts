f03d183e5c778459395007c5baaf3b48bb47391f
unknown <mohammedyosef712@gmail.com>
2023-12-01 15:34:29 -0800

    change start to start:prod

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  console.log('hereeee');
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
